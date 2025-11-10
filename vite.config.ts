c62fb4eec99b3109ca6eb7bb66d07b1985418f22
Ngoc Ngo <ngocnv371@gmail.com>
2025-11-09 20:29:44 -0800

    update zalo deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,25 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import zalo from "zmp-vite-plugin";
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
+  const env = loadEnv(mode, ".", "");
+  return {
+    server: {
+      port: 3000,
+      host: "0.0.0.0",
+    },
+    plugins: [react(), zalo()],
+    base: "./",
+    define: {
+      "process.env.SUPABASE_URL": JSON.stringify(env.SUPABASE_URL),
+      "process.env.SUPABASE_ANON_KEY": JSON.stringify(env.SUPABASE_ANON_KEY),
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "."),
       },
-      plugins: [react()],
-      define: {
-        'process.env.SUPABASE_URL': JSON.stringify(env.SUPABASE_URL),
-        'process.env.SUPABASE_ANON_KEY': JSON.stringify(env.SUPABASE_ANON_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+    },
+  };
 });
