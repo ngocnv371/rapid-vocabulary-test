44fcbaefb8df9f54d777e10c26e15e9fc2f327d4
Ngoc Ngo <ngocnv371@gmail.com>
2025-11-09 23:49:26 -0800

    correct color

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,11 +12,6 @@ module.exports = {
     extend: {
       colors: {
         primary: "var(--zmp-primary-color)",
-        gray: "#767A7F",
-        divider: "#E9EBED",
-        green: "#288F4E",
-        background: "#ffffff",
-        skeleton: "rgba(0, 0, 0, 0.1)",
       },
     },
   },
