f8c2349b9965b369e81782286080579c7dd7f011
Ngoc Ngo <ngocnv371@gmail.com>
2025-12-23 20:06:47 +0700

    add assets

--- landing/src/components/icons.tsx
+++ landing/src/components/icons.tsx
@@ -1,12 +1,12 @@
-
 import React from 'react';
+import logoImage from '../assets/logo-x512.png';
 
 type IconProps = {
   className?: string;
 };
 
 export const BrainCircuitIcon: React.FC<IconProps> = (props) => (
-  "🧠"
+  <img src={logoImage} alt="Brain Circuit" {...props} />
 );
 
 export const ArrowRightIcon: React.FC<IconProps> = (props) => (
@@ -41,13 +41,19 @@ export const BookOpenIcon: React.FC<IconProps> = (props) => (
 );
 
 export const TrophyIcon: React.FC<IconProps> = (props) => (
-  "🌍"
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 18.75h-9m9 0a3 3 0 013 3h-15a3 3 0 013-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 01-.982-3.172M9.497 14.25a7.454 7.454 0 00.981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 007.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 002.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 012.916.52 6.003 6.003 0 01-5.395 4.972m0 0a6.726 6.726 0 01-2.749 1.35m0 0a6.772 6.772 0 01-3.044 0" />
+  </svg>
 );
 
 export const AppleIcon: React.FC<IconProps> = (props) => (
-  "🍎"
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
+    <path d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.81-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M13 3.5c.73-.83 1.94-1.46 2.94-1.5.13 1.17-.34 2.35-1.04 3.19-.69.85-1.83 1.51-2.95 1.42-.15-1.15.41-2.35 1.05-3.11z" />
+  </svg>
 );
 
 export const GooglePlayIcon: React.FC<IconProps> = (props) => (
-  "🤖"
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
+    <path d="M3 20.5v-17c0-.59.34-1.11.84-1.35L13.69 12l-9.85 9.85c-.5-.24-.84-.76-.84-1.35m13.81-5.38L6.05 21.34l8.49-8.49 2.27 2.27m.61-10.78l-2.27 2.27-8.49-8.49L17.42 4.34c.48.26.79.75.79 1.29 0 .54-.31 1.03-.79 1.29m-10.37 12L13.69 12 3.84 2.15c.5-.24 1.1-.16 1.54.18l10.08 5.67-2.26 2.27-6.84 3.63z" />
+  </svg>
 );
